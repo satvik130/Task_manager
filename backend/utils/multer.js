b75a7d86751066681ed462c81dd3c48d38e19852
Satvik Mishra <148708228+satvik130@users.noreply.github.com>
2025-08-01 22:54:03 +0530

    Update multer.js

--- backend/utils/multer.js
+++ backend/utils/multer.js
@@ -4,7 +4,7 @@ const path = require("path");
 // Storage configuration
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/"); // Make sure 'uploads' folder exists
+    cb(null, "uploads/"); 
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
