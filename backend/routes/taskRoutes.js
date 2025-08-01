0311555a38d2e69d5caf433443c0943c7c690a1d
Satvik Mishra <148708228+satvik130@users.noreply.github.com>
2025-08-01 22:52:54 +0530

    Update taskRoutes.js

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -11,24 +11,16 @@ const upload = require("../utils/multer");
 
 const router = express.Router();
 
-// @route   POST /api/tasks/
-// @desc    Create a new task
-// @access  Private
+
 router.post("/", protect, upload.array("documents", 5), createTask);
 
-// @route   GET /api/tasks/
-// @desc    Get all tasks of logged in user
-// @access  Private
+
 router.get("/", protect, getTasks);
 
-// @route   PUT /api/tasks/:id
-// @desc    Update a task
-// @access  Private
+
 router.put("/:id", protect, upload.array("documents", 5), updateTask);
 
-// @route   DELETE /api/tasks/:id
-// @desc    Delete a task
-// @access  Private
+
 router.delete("/:id", protect, deleteTask);
 
 module.exports = router;
