ecd3d830f1e6405ec98c3d8482f9866ad159c9f8
Satvik Mishra <148708228+satvik130@users.noreply.github.com>
2025-08-01 22:51:54 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -23,7 +23,7 @@ const userSchema = new mongoose.Schema({
   },
 }, { timestamps: true });
 
-// Hash password before saving
+
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   const salt = await bcrypt.genSalt(10);
@@ -31,7 +31,7 @@ userSchema.pre("save", async function (next) {
   next();
 });
 
-// Compare password method
+
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
