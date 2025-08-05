d035e7e3d3c27783783f4885705787cc25b95d29
Satvik mishra <mishrasatvik130@gmail.com>
2025-08-06 00:07:49 +0530

    fix: updated axios baseURL to Render backend URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:4000/api',
-  withCredentials: true, // sends cookies for auth
+  baseURL: `${process.env.REACT_APP_BASE_URL}/api`,
+  withCredentials: true,
 });
 
 export default API;
